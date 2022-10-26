71a8df04957ee705b4e79fda47e7ac691f225cda
Kate3587 <108617406+Kate3587@users.noreply.github.com>
2022-10-26 22:58:37 +0200

    htm input phone

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -7,7 +7,7 @@ class LoginForm extends Component{
         return(
         <form>
             <div>
-                <label for="input Name">
+                <label htmlFor="input Name">
                     Name
                 </label>
                 <input
@@ -15,6 +15,13 @@ class LoginForm extends Component{
                     name="name"
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                />
+                <input
+                    type="tel"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     required
                 />
                 <button type="button">Add Contact</button>
